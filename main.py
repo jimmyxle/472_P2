4c3851f9e12d6d8f90c0d2c66d9b5b2205ca2fa7
Jimmy Le <jimmyle77@hotmail.com>
2019-11-30 21:42:27 -0500

    minor text fixes

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import NaivBayesClassifier
 import sys
 if __name__ == '__main__':
-
-    print(sys.argv)
-
     all_documents, all_labels = NaivBayesClassifier.read_Document("dataFile.txt")
     split_point = int(0.80 * len(all_documents))
     training_documents = all_documents[:split_point]
@@ -15,20 +12,26 @@ if __name__ == '__main__':
 
     trainedData, label_probability = NaivBayesClassifier.train_nb(training_documents, training_label)
 
-    if sys.argv > 1:
-        print("testing file ", sys.argv[1])
-        test_doc, test_label = NaivBayesClassifier.read_Document(sys.argv[1])
-        print('logarithmic Score for `pos` label = ',
-              NaivBayesClassifier.score_doc_label(test_doc, 'pos', trainedData, label_probability))
-        print('logarithmic Score for `neg` label = ',
-              NaivBayesClassifier.score_doc_label(test_doc, 'neg', trainedData, label_probability))
-
+    if len(sys.argv) > 2 and sys.argv[1] == "-d":
+        print("Testing file ", sys.argv[2])
+        test_doc, test_label = NaivBayesClassifier.read_Document(sys.argv[2])
         guessedLabels = NaivBayesClassifier.classify_documents(test_doc, trainedData, label_probability)
+        print("Accuracy = ", NaivBayesClassifier.accuracy(test_label, guessedLabels))
+    elif sys.argv[1] == "-m":
+        print("Testing message = ", sys.argv[2])
+        print("positive log score = ", NaivBayesClassifier.score_doc_label(sys.argv[2], 'pos', trainedData, label_probability))
+        print("negative log score = ", NaivBayesClassifier.score_doc_label(sys.argv[2], 'neg', trainedData, label_probability))
+        print("This document is probably = ", NaivBayesClassifier.classify_nb(sys.argv[2], trainedData, label_probability))
 
-        print('Accuracy of classifier :', NaivBayesClassifier.accuracy(test_label, guessedLabels))
+    print('Accuracy of detecting a positive class:',
+          NaivBayesClassifier.accuracyOfClassDetection(evaluation_labels, guessedLabels, 'pos'))
+    print('Accuracy of detecting a negative class:',
+          NaivBayesClassifier.accuracyOfClassDetection(evaluation_labels, guessedLabels, 'neg'))
 
-
-
+    print('Accuracy of guessed positive classes:',
+          NaivBayesClassifier.accuracyOfGuessedClass(evaluation_labels, guessedLabels, 'pos'))
+    print('Accuracy of guessed negative classes:',
+          NaivBayesClassifier.accuracyOfGuessedClass(evaluation_labels, guessedLabels, 'neg'))
 
 
 
